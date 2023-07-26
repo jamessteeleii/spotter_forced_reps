2220204e4fc4bbee9195e3770066c67f6a468514
James Steele <james.steele@solent.ac.uk>
2023-07-26 16:57:25 +0100

    First pipeline finished - read data, individual plot, fit model

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,51 @@
 get_data <- function(file) {
-  read.csv("data/data forced rep study.csv") %>%
-    mutate(sex = as.factor(sex))
+  read.csv(file, fileEncoding = 'UTF-8-BOM') %>% 
+    mutate(sex = as.factor(sex),
+           actual_assistance_percentage = (actual_assistance_kg / load_kg)*100,
+           avg_tut_rep = tut_set/reps) %>%
+    pivot_longer(c(trainee_perception_assistance, spotter_perception_assistance),
+                 names_to = "role", values_to = "perception_assistance") %>%
+    mutate(role = str_remove(role, "_perception_assistance"),
+           id = if_else(role == "trainee", trainee_id, spotter_id))
 }
 
+plot_individual_data <- function(data) {
+  
+  individual_data_plot <- data %>%
+    ggplot(aes(x = actual_assistance_percentage, y = perception_assistance, group=role, color=role)) +
+    geom_point(position = position_jitter(width = 0.5, height = 0.5)) +
+    geom_line() +
+    geom_text(aes(label=forced_rep_no, y = perception_assistance+10), size = 2.5, show.legend = FALSE) +
+    facet_wrap(~id, ncol = 9) +
+    scale_x_continuous(breaks = c(0,25,50,75,100)) +
+    scale_y_continuous(breaks = c(0,25,50,75,100)) +
+    labs(x = "Actual Assistance Provided (%)",
+         y = "Perception of Assistance Provided (%)",
+         color = "Role",
+         title = "Individual Participant Data",
+         subtitle = "Note, the labels indicate which forced repetition number each point corresponds to") +
+    theme_bw() +
+    theme(legend.position = "bottom")
+  
+  individual_data_plot
+  
+  ggsave("plots/disability_rsa.tiff", width = 7.5, height = 7.5, device = "tiff", dpi = 300)
+  
+}
+
+fit_brms_model <- function(data) {
+  
+  # run rstan quicker - for bayesian beta regression later on
+  rstan_options(auto_write = TRUE)
+  options(mc.cores = parallel::detectCores()-1)
+  
+  
+  model_brms <- brm(perception_assistance ~ actual_assistance_percentage * role + forced_rep_no + actual_assistance_percentage:forced_rep_no +
+                      (actual_assistance_percentage * role + forced_rep_no + actual_assistance_percentage:forced_rep_no | id),
+                    data=data,
+                    chains = 4,
+                    iter = 3000, warmup = 1000,
+                    cores = 4,
+                    control = list(adapt_delta = 0.95), init = 0)
+  
+}
--- _targets.R
+++ _targets.R
@@ -1,17 +1,15 @@
 # _targets.R file
 library(targets)
 source("R/functions.R")
-tar_option_set(packages = c("tidyverse", "lme4", "purrr", "base", "ggeffects", "patchwork"))
+tar_option_set(packages = c("tidyverse", "rstan", "brms", "base"))
 
-# list(
-#   # Load in data
-#   tar_target(rsa_file, "rsa_data.csv", format = "file"),
-#   tar_target(sprint_file, "sprint_data.csv", format = "file"),
-#   tar_target(rsa_data, get_rsa_data(rsa_file)),
-#   # tar_target(sprint_data, get_sprint_data(sprint_file)),
-#   
-#   # Fit models for repeated sprint ability (rsa) by disability and classification
-#   tar_target(disability_rsa_models, fit_disability_rsa_models(rsa_data)),
+list(
+  # Load in data
+  tar_target(file, "data/data forced rep study.csv", format = "file"),
+  tar_target(data, get_data(file)),
+
+  # Fit model
+  tar_target(model_brms, fit_brms_model(data)),
 #   tar_target(classif_rsa_models, fit_classif_rsa_models(rsa_data)),
 #   
 #   # Get model summaries
@@ -22,8 +20,9 @@ tar_option_set(packages = c("tidyverse", "lme4", "purrr", "base", "ggeffects", "
 #   tar_target(disability_rsa_predicted_values, get_disability_rsa_predicted_values(disability_rsa_models)),
 #   tar_target(classif_rsa_predicted_values, get_classif_rsa_predicted_values(classif_rsa_models)),
 #   
-#   # Make plots
-#   tar_target(disability_rsa_plot, make_disability_rsa_plot(rsa_data, disability_rsa_predicted_values)),
+  # Make plots
+  tar_target(individual_data_plot, plot_individual_data(data))
 #   tar_target(classif_rsa_plot, make_classif_rsa_plot(rsa_data, classif_rsa_predicted_values))
 #   
-# )
+)
+
