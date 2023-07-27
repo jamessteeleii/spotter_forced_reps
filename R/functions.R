e007f16b642301e57a09851e9a5a3a433d6d53dd
James Steele <james.steele@solent.ac.uk>
2023-07-27 19:28:21 +0100

    Analysis pipeline finished, report begun

--- R/functions.R
+++ R/functions.R
@@ -2,8 +2,7 @@ get_data <- function(file) {
   read.csv("data/data forced rep study.csv") %>%
   mutate(sex = as.factor(sex),
          actual_assistance_percentage = (actual_assistance_kg / load_kg)*100, # kept on % scale to aid coefficient interpretation
-         avg_tut_rep = tut_set/reps,
-         forced_rep_no = forced_rep_no-1.5) %>% # forced rep number centred to aid coefficient interpretation
+         avg_tut_rep = tut_set/reps) %>% 
     pivot_longer(c(trainee_perception_assistance, spotter_perception_assistance),
                  names_to = "role", values_to = "perception_assistance") %>%
     mutate(role = str_remove(role, "_perception_assistance"),
@@ -31,13 +30,14 @@ plot_individual_data <- function(data) {
     theme_bw() +
     theme(legend.position = "bottom")
   
-  individual_data_plot
-  
-  ggsave("plots/individual_data_plot.tiff", width = 10, height = 10, device = "tiff", dpi = 300)
+  ggsave("plots/individual_data_plot.tiff", individual_data_plot, width = 10, height = 10, device = "tiff", dpi = 300)
   
 }
 
 fit_brms_model <- function(data) {
+  
+  data <- data %>%
+    mutate(forced_rep_no = forced_rep_no-1.5) # centred to aid interpretation of actual assistance slope
   
   # run rstan quicker - for bayesian beta regression later on
   rstan_options(auto_write = TRUE)
@@ -68,20 +68,20 @@ make_pp_check <- function(model_brms) {
   pp_check(model_brms)
 }
 
-
 plot_model_data <- function(model_brms, data) {
   posterior_epred_draws <- crossing(actual_assistance_percentage = seq(from = 0, to = max(data$actual_assistance_percentage), length = max(data$actual_assistance_percentage)),
                                     id = unique(data$id),
                                     role = unique(data$role),
-                                    forced_rep_no = unique(data$forced_rep_no)) %>%
-    add_epred_draws(model_brms, re_formula = NA) 
+                                    forced_rep_no = c(-0.5,0.5)) %>%
+    add_epred_draws(model_brms, re_formula = NA, ndraws = 4000) 
   
   model_data_plot <- posterior_epred_draws %>%
-    mutate(rep_label = "Forced Repetition Number") %>%
+    mutate(rep_label = "Forced Repetition Number",
+           forced_rep_no = if_else(forced_rep_no == -0.5, 1, 2)) %>%
     ggplot(aes(x = actual_assistance_percentage, y = .epred*100, color=role, fill=role)) +
     geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
     geom_point(data=data, aes(x = actual_assistance_percentage, y = perception_assistance*100, color=role, fill=role),
-               position = position_jitter(width = 0.5, height = 0.5)) +
+               position = position_jitter(width = 0.5, height = 0.5), alpha=0.75) +
     stat_lineribbon(aes(y = .epred*100), .width = .95, alpha = 0.25, show.legend=TRUE) +
     scale_fill_brewer(palette = "Set2") +
     scale_color_brewer(palette = "Dark2") +
@@ -97,9 +97,7 @@ plot_model_data <- function(model_brms, data) {
     theme_bw() + 
     theme(panel.grid=element_blank())
   
-  model_data_plot
-  
-  ggsave("plots/model_data_plot.tiff", width = 10, height = 5, device = "tiff", dpi = 300)
+  ggsave("plots/model_data_plot.tiff", model_data_plot, width = 10, height = 5, device = "tiff", dpi = 300)
   
 }
 
--- _targets.R
+++ _targets.R
@@ -1,8 +1,8 @@
 # _targets.R file
 library(targets)
-# library(tarchetypes)
+library(tarchetypes)
 source("R/functions.R")
-tar_option_set(packages = c("tidyverse", "rstan", "brms", "base", "tidybayes", "ggh4x", "broom.mixed"))
+tar_option_set(packages = c("tidyverse", "rstan", "brms", "base", "tidybayes", "ggh4x", "broom.mixed", "quarto"))
 
 list(
   # Load in data
@@ -12,13 +12,17 @@ list(
   # Fit model
   tar_target(model_brms, fit_brms_model(data)),
 
-  # Make plots
+  # Make and save plots
   tar_target(individual_data_plot, plot_individual_data(data)),
-  tar_target(trace_plots, make_trace_plots(model_brms)),
-  tar_target(pp_check_plot, make_pp_check(model_brms)),
   tar_target(model_data_plot, plot_model_data(model_brms, data)),
 
+  tar_target(trace_plots, make_trace_plots(model_brms)),
+  tar_target(pp_check_plot, make_pp_check(model_brms)),
+  
   # Get tidy model summary
-  tar_target(tidy_model_brms, get_tidy_model(model_brms))
+  tar_target(tidy_model_brms, get_tidy_model(model_brms)),
+  
+  # Render the report
+  tar_quarto(report, "report.qmd")
+  
 )
-
